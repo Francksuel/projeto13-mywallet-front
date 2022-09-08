cd2fb1744dc58671218bf0357206568c4712de83
Francksuel <francksuelbarbosa@gmail.com>
2022-09-08 14:20:31 -0300

    Cadaster screen

--- src/components/Cadaster.js
+++ src/components/Cadaster.js
@@ -1,18 +1,90 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Input from "./common/Input";
+import { useState } from "react";
+import Button from "./common/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { postCadaster } from "../services/myWallet";
 
-export default function Cadaster(){
-    return (
-        <Logo><p>Cadaster</p></Logo>
-    )
+export default function Cadaster() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
+	const navigate = useNavigate();
+
+	function registration(e) {
+		e.preventDefault();
+        if (password !== passwordConfirm){
+            return alert ("As senhas digitadas não coincidem!");
+        }
+		const dataLog = {
+            name,
+			email,
+			password
+		};
+		const request = postCadaster(dataLog);
+		request.catch((error) => {
+			alert(error.response.data);
+		});
+		request.then((res) => {		
+            alert ("Usuário cadastrado com sucesso!");	
+			navigate("/");
+		});
+	}
+
+	return (
+		<Wrapper>
+			<h1>MyWallet</h1>
+			<form onSubmit={registration}>
+				<Input
+					placeholder={"Nome"}
+					type={"text"}
+					value={name}
+					disabled={false}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<Input
+					placeholder={"E-mail"}
+					type={"email"}
+					value={email}
+					disabled={false}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Input
+					placeholder={"Senha"}
+					type={"password"}
+					value={password}
+					disabled={false}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<Input
+					placeholder={"Confirme a senha"}
+					type={"password"}
+					value={passwordConfirm}
+					disabled={false}
+					onChange={(e) => setPasswordConfirm(e.target.value)}
+				/>
+				<Button value={"Cadastrar"} />
+			</form>
+			<Link to={"/"}>
+				<h2>Já tem uma conta? Entre agora!</h2>
+			</Link>
+		</Wrapper>
+	);
 }
 
-const Logo = styled.div`
-position: fixed;
-background-color: red;
-width: 60px;
-height: 60px;
-top: 0;
-left: 0;
-font-size: 20px;
-color: white;
-`
+const Wrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	h1 {
+		margin: 95px 0 24px 0;
+	}
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+`;
--- src/services/myWallet.js
+++ src/services/myWallet.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 const URL = "http://localhost:5000/";
 
+function postCadaster(body) {
+    const promise = axios.post(`${URL}sign-up`, body);
+    return promise;
+}
+
 function postLogin(body) {
     const promise = axios.post(`${URL}sign-in`, body);
     return promise;
 }
 
-export {postLogin};
+
+export {postLogin, postCadaster};
